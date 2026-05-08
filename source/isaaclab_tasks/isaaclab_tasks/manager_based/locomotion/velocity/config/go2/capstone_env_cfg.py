f8fe401fd3e859fa34c4aa4af3de74e87a42f883
Glen02lee <glen@handong.ac.kr>
2026-05-08 20:57:19 +0900

    test: revert initial spawn height back to -1.0m per user request

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/capstone_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/capstone_env_cfg.py
@@ -51,8 +51,8 @@ class UnitreeGo2CapstoneEnvCfg(UnitreeGo2RoughEnvCfg):
         )
 
         # --- [로봇 시작 위치 설정] ---
-        # 복도 메쉬의 원점(0,0,0)에서 로봇이 안전하게 착지하도록 Z축만 -3.0m 설정
-        self.scene.robot.init_state.pos = (0.0, 0.0, -3.0) 
+        # 복도 메쉬의 원점(0,0,0)에서 로봇이 안전하게 착지하도록 Z축만 -1.0m 설정
+        self.scene.robot.init_state.pos = (0.0, 0.0, -1.0) 
 
         # 지형 스캔 관련 불필요한 기능 끄기
         self.scene.height_scanner = None
