5edab871e647f7dce024904bdcf8ea2772b2b55b
Glen02lee <glen@handong.ac.kr>
2026-05-08 20:27:22 +0900

    test: reduce initial spawn height to 1m for drop test in capstone env

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/capstone_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/capstone_env_cfg.py
@@ -51,8 +51,8 @@ class UnitreeGo2CapstoneEnvCfg(UnitreeGo2RoughEnvCfg):
         )
 
         # --- [로봇 시작 위치 설정] ---
-        # 10m 상공에서 떨어뜨리는 낙하 테스트용 시작 높이 (원래 -1.0m에서 +10m)
-        self.scene.robot.init_state.pos = (0.0, 0.0, 9.0) 
+        # 1m 상공에서 떨어뜨리는 낙하 테스트용 시작 높이 (원래 -1.0m에서 +1m)
+        self.scene.robot.init_state.pos = (0.0, 0.0, 0.0) 
 
         # 지형 스캔 관련 불필요한 기능 끄기
         self.scene.height_scanner = None
